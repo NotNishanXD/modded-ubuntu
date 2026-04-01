2e750ba063eca94da1ec53386a0341196317c994
M4rkyyyyy. <sahanisha238@gmail.com>
2026-04-01 07:36:08 +0000

    Update gui.sh

--- distro/gui.sh
+++ distro/gui.sh
@@ -1,290 +1,149 @@
 #!/bin/bash
 
-R="$(printf '\033[1;31m')"
-G="$(printf '\033[1;32m')"
-Y="$(printf '\033[1;33m')"
-W="$(printf '\033[1;37m')"
-C="$(printf '\033[1;36m')"
-arch=$(uname -m)
-username=$(getent group sudo | awk -F ':' '{print $4}' | cut -d ',' -f1)
+# Colors
+R="\033[1;31m"
+G="\033[1;32m"
+Y="\033[1;33m"
+C="\033[1;36m"
+W="\033[1;37m"
 
-check_root(){
-	if [ "$(id -u)" -ne 0 ]; then
-		echo -ne " ${R}Run this program as root!\n\n"${W}
-		exit 1
-	fi
-}
+CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
+UBUNTU_DIR="$PREFIX/var/lib/proot-distro/installed-rootfs/ubuntu"
 
 banner() {
 	clear
-	cat <<- EOF
-		${Y}    _  _ ___  _  _ _  _ ___ _  _    _  _ ____ ___  
-		${C}    |  | |__] |  | |\ |  |  |  |    |\/| |  | |  \ 
-		${G}    |__| |__] |__| | \|  |  |__|    |  | |__| |__/ 
-
-	EOF
-	echo -e "${G}     A modded gui version of ubuntu for Termux\n"
+	echo -e "${Y}
+    _  _ ___  _  _ _  _ ___ _  _    _  _ ____ ___  
+${C}    |  | |__] |  | |\ |  |  |  |    |\/| |  | |  \ 
+${G}    |__| |__] |__| | \|  |  |__|    |  | |__| |__/ 
+${W}
+     A modded gui version of ubuntu for Termux
+"
 }
 
-note() {
-	banner
-	echo -e " ${G} [-] Successfully Installed !\n"${W}
-	sleep 1
-	cat <<- EOF
-		 ${G}[-] Type ${C}vncstart${G} to run Vncserver.
-		 ${G}[-] Type ${C}vncstop${G} to stop Vncserver.
-
-		 ${C}Install VNC VIEWER Apk on your Device.
-
-		 ${C}Open VNC VIEWER & Click on + Button.
-
-		 ${C}Enter the Address localhost:1 & Name anything you like.
-
-		 ${C}Set the Picture Quality to High for better Quality.
-
-		 ${C}Click on Connect & Input the Password.
-
-		 ${C}Enjoy :D${W}
-	EOF
+pkg_install() {
+	for pkg in "$@"; do
+		if ! command -v "$pkg" >/dev/null 2>&1; then
+			echo -e "${G}Installing ${Y}$pkg${W}"
+			pkg install -y "$pkg"
+		else
+			echo -e "${Y}$pkg already installed${W}"
+		fi
+	done
 }
 
 package() {
 	banner
-	echo -e "${R} [${W}-${R}]${C} Checking required packages..."${W}
-	apt-get update -y
-	apt install udisks2 -y
-	rm /var/lib/dpkg/info/udisks2.postinst
-	echo "" > /var/lib/dpkg/info/udisks2.postinst
-	dpkg --configure -a
-	apt-mark hold udisks2
-	
-	packs=(sudo gnupg2 curl nano git xz-utils at-spi2-core xfce4 xfce4-goodies xfce4-terminal librsvg2-common menu inetutils-tools dialog exo-utils tigervnc-standalone-server tigervnc-common tigervnc-tools dbus-x11 fonts-beng fonts-beng-extra gtk2-engines-murrine gtk2-engines-pixbuf apt-transport-https)
-	for hulu in "${packs[@]}"; do
-		type -p "$hulu" &>/dev/null || {
-			echo -e "\n${R} [${W}-${R}]${G} Installing package : ${Y}$hulu${W}"
-			apt-get install "$hulu" -y --no-install-recommends
-		}
-	done
-	
-	apt-get update -y
-	apt-get upgrade -y
-}
+	echo -e "${C}Checking required packages...${W}"
 
-install_apt() {
-	for apt in "$@"; do
-		[[ `command -v $apt` ]] && echo "${Y}${apt} is already Installed!${W}" || {
-			echo -e "${G}Installing ${Y}${apt}${W}"
-			apt install -y ${apt}
-		}
-	done
-}
-
-install_vscode() {
-	[[ $(command -v code) ]] && echo "${Y}VSCode is already Installed!${W}" || {
-		echo -e "${G}Installing ${Y}VSCode${W}"
-		curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-		install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-		echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
-		apt update -y
-		apt install code -y
-		echo "Patching.."
-		curl -fsSL https://raw.githubusercontent.com/modded-ubuntu/modded-ubuntu/master/patches/code.desktop > /usr/share/applications/code.desktop
-		echo -e "${C} Visual Studio Code Installed Successfully\n${W}"
-	}
-}
-
-install_sublime() {
-	[[ $(command -v subl) ]] && echo "${Y}Sublime is already Installed!${W}" || {
-		apt install gnupg2 software-properties-common --no-install-recommends -y
-		echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
-		curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/sublime.gpg 2> /dev/null
-		apt update -y
-		apt install sublime-text -y 
-		echo -e "${C} Sublime Text Editor Installed Successfully\n${W}"
-	}
-}
-
-install_chromium() {
-	[[ $(command -v chromium) ]] && echo "${Y}Chromium is already Installed!${W}\n" || {
-		echo -e "${G}Installing ${Y}Chromium${W}"
-		apt purge chromium* chromium-browser* snapd -y
-		apt install gnupg2 software-properties-common --no-install-recommends -y
-		echo -e "deb http://ftp.debian.org/debian buster main\ndeb http://ftp.debian.org/debian buster-updates main" >> /etc/apt/sources.list
-		apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DCC9EFBF77E11517
-		apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
-		apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50
-		apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
-		apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
-		apt update -y
-		apt install chromium -y
-		sed -i 's/chromium %U/chromium --no-sandbox %U/g' /usr/share/applications/chromium.desktop
-		echo -e "${G} Chromium Installed Successfully\n${W}"
-	}
-}
-
-install_firefox() {
-	[[ $(command -v firefox) ]] && echo "${Y}Firefox is already Installed!${W}\n" || {
-		echo -e "${G}Installing ${Y}Firefox${W}"
-		bash <(curl -fsSL "https://raw.githubusercontent.com/modded-ubuntu/modded-ubuntu/master/distro/firefox.sh")
-		echo -e "${G} Firefox Installed Successfully\n${W}"
-	}
-}
-
-install_softwares() {
-	banner
-	cat <<- EOF
-		${Y} ---${G} Select Browser ${Y}---
-
-		${C} [${W}1${C}] Firefox (Default)
-		${C} [${W}2${C}] Chromium
-		${C} [${W}3${C}] Both (Firefox + Chromium)
-
-	EOF
-	read -n1 -p "${R} [${G}~${R}]${Y} Select an Option: ${G}" BROWSER_OPTION
-	banner
-
-	[[ ("$arch" != 'armhf') || ("$arch" != *'armv7'*) ]] && {
-		cat <<- EOF
-			${Y} ---${G} Select IDE ${Y}---
-
-			${C} [${W}1${C}] Sublime Text Editor (Recommended)
-			${C} [${W}2${C}] Visual Studio Code
-			${C} [${W}3${C}] Both (Sublime + VSCode)
-			${C} [${W}4${C}] Skip! (Default)
-
-		EOF
-		read -n1 -p "${R} [${G}~${R}]${Y} Select an Option: ${G}" IDE_OPTION
-		banner
-	}
-	
-	cat <<- EOF
-		${Y} ---${G} Media Player ${Y}---
-
-		${C} [${W}1${C}] MPV Media Player (Recommended)
-		${C} [${W}2${C}] VLC Media Player
-		${C} [${W}3${C}] Both (MPV + VLC)
-		${C} [${W}4${C}] Skip! (Default)
-
-	EOF
-	read -n1 -p "${R} [${G}~${R}]${Y} Select an Option: ${G}" PLAYER_OPTION
-	{ banner; sleep 1; }
-
-	if [[ ${BROWSER_OPTION} == 2 ]]; then
-		install_chromium
-	elif [[ ${BROWSER_OPTION} == 3 ]]; then
-		install_firefox
-		install_chromium
-	else
-		install_firefox
+	# Storage
+	if [ ! -d "$HOME/storage" ]; then
+		echo -e "${C}Setting up storage...${W}"
+		termux-setup-storage
 	fi
 
-	[[ ("$arch" != 'armhf') || ("$arch" != *'armv7'*) ]] && {
-		if [[ ${IDE_OPTION} == 1 ]]; then
-			install_sublime
-		elif [[ ${IDE_OPTION} == 2 ]]; then
-			install_vscode
-		elif [[ ${IDE_OPTION} == 3 ]]; then
-			install_sublime
-			install_vscode
+	# Update once (not spam)
+	pkg update -y && pkg upgrade -y
+
+	pkg_install pulseaudio proot-distro curl
+}
+
+distro() {
+	echo -e "\n${C}Checking Ubuntu distro...${W}"
+
+	if [ -d "$UBUNTU_DIR" ]; then
+		echo -e "${Y}Ubuntu already installed, skipping.${W}"
+		return
+	fi
+
+	echo -e "${G}Installing Ubuntu...${W}"
+	proot-distro install ubuntu || {
+		echo -e "${R}Failed to install Ubuntu${W}"
+		exit 1
+	}
+
+	echo -e "${G}Ubuntu installed successfully${W}"
+}
+
+sound() {
+	echo -e "\n${C}Fixing sound...${W}"
+
+	SOUND_FILE="$HOME/.sound"
+
+	# Prevent duplicate lines
+	grep -qxF "pulseaudio --start --exit-idle-time=-1" "$SOUND_FILE" 2>/dev/null || \
+	echo "pulseaudio --start --exit-idle-time=-1" >> "$SOUND_FILE"
+
+	grep -qxF "pacmd load-module module-aaudio-sink" "$SOUND_FILE" 2>/dev/null || \
+	echo "pacmd load-module module-aaudio-sink" >> "$SOUND_FILE"
+
+	grep -qxF "pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" "$SOUND_FILE" 2>/dev/null || \
+	echo "pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" >> "$SOUND_FILE"
+}
+
+downloader() {
+	local path="$1"
+	local url="$2"
+
+	echo -e "${C}Downloading $(basename "$path")...${W}"
+	curl -L --retry 3 --retry-delay 2 -o "$path" "$url"
+}
+
+setup_vnc() {
+	echo -e "\n${C}Setting up VNC scripts...${W}"
+
+	for file in vncstart vncstop; do
+		if [ -f "$CURR_DIR/distro/$file" ]; then
+			cp "$CURR_DIR/distro/$file" "$UBUNTU_DIR/usr/local/bin/$file"
 		else
-			echo -e "${Y} [!] Skipping IDE Installation\n"
-			sleep 1
+			downloader "$CURR_DIR/$file" "https://raw.githubusercontent.com/modded-ubuntu/modded-ubuntu/master/distro/$file"
+			mv "$CURR_DIR/$file" "$UBUNTU_DIR/usr/local/bin/$file"
 		fi
-	}
+		chmod +x "$UBUNTU_DIR/usr/local/bin/$file"
+	done
+}
 
-	if [[ ${PLAYER_OPTION} == 1 ]]; then
-		install_apt "mpv"
-	elif [[ ${PLAYER_OPTION} == 2 ]]; then
-		install_apt "vlc"
-	elif [[ ${PLAYER_OPTION} == 3 ]]; then
-		install_apt "mpv" "vlc"
+permission() {
+	banner
+	echo -e "${C}Setting up environment...${W}"
+
+	# user.sh
+	if [ -f "$CURR_DIR/distro/user.sh" ]; then
+		cp "$CURR_DIR/distro/user.sh" "$UBUNTU_DIR/root/user.sh"
 	else
-		echo -e "${Y} [!] Skipping Media Player Installation\n"
-		sleep 1
+		downloader "$CURR_DIR/user.sh" "https://raw.githubusercontent.com/modded-ubuntu/modded-ubuntu/master/distro/user.sh"
+		mv "$CURR_DIR/user.sh" "$UBUNTU_DIR/root/user.sh"
 	fi
 
+	chmod +x "$UBUNTU_DIR/root/user.sh"
+
+	setup_vnc
+
+	# timezone
+	getprop persist.sys.timezone > "$UBUNTU_DIR/etc/timezone"
+
+	# launcher
+	echo "proot-distro login ubuntu" > "$PREFIX/bin/ubuntu"
+	chmod +x "$PREFIX/bin/ubuntu"
+
+	if [ -f "$PREFIX/bin/ubuntu" ]; then
+		banner
+		echo -e "${G}Ubuntu CLI installed successfully!${W}
+
+${C}Commands:
+${G}ubuntu        ${W}-> Start Ubuntu
+${G}bash user.sh ${W}-> Start GUI (inside Ubuntu)
+
+${Y}Restart Termux if anything breaks.${W}
+"
+	else
+		echo -e "${R}Something went wrong creating launcher${W}"
+		exit 1
+	fi
 }
 
-downloader(){
-	path="$1"
-	[[ -e "$path" ]] && rm -rf "$path"
-	echo "Downloading $(basename $1)..."
-	curl --progress-bar --insecure --fail \
-		 --retry-connrefused --retry 3 --retry-delay 2 \
-		  --location --output ${path} "$2"
-}
-
-sound_fix() {
-	echo "$(echo "bash ~/.sound" | cat - /data/data/com.termux/files/usr/bin/ubuntu)" > /data/data/com.termux/files/usr/bin/ubuntu
-	echo "export DISPLAY=":1"" >> /etc/profile
-	echo "export PULSE_SERVER=127.0.0.1" >> /etc/profile 
-	source /etc/profile
-}
-
-rem_theme() {
-	theme=(Bright Daloa Emacs Moheli Retro Smoke)
-	for rmi in "${theme[@]}"; do
-		type -p "$rmi" &>/dev/null || {
-			rm -rf /usr/share/themes/"$rmi"
-		}
-	done
-}
-
-rem_icon() {
-	fonts=(hicolor LoginIcons ubuntu-mono-light)
-	for rmf in "${fonts[@]}"; do
-		type -p "$rmf" &>/dev/null || {
-			rm -rf /usr/share/icons/"$rmf"
-		}
-	done
-}
-
-config() {
-	banner
-	sound_fix
-
-	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
-	yes | apt upgrade
-	yes | apt install gtk2-engines-murrine gtk2-engines-pixbuf sassc optipng inkscape libglib2.0-dev-bin
-	mv -vf /usr/share/backgrounds/xfce/xfce-verticals.png /usr/share/backgrounds/xfce/xfceverticals-old.png
-	temp_folder=$(mktemp -d -p "$HOME")
-	{ banner; sleep 1; cd $temp_folder; }
-
-	echo -e "${R} [${W}-${R}]${C} Downloading Required Files..\n"${W}
-	downloader "fonts.tar.gz" "https://github.com/modded-ubuntu/modded-ubuntu/releases/download/config/fonts.tar.gz"
-	downloader "icons.tar.gz" "https://github.com/modded-ubuntu/modded-ubuntu/releases/download/config/icons.tar.gz"
-	downloader "wallpaper.tar.gz" "https://github.com/modded-ubuntu/modded-ubuntu/releases/download/config/wallpaper.tar.gz"
-	downloader "gtk-themes.tar.gz" "https://github.com/modded-ubuntu/modded-ubuntu/releases/download/config/gtk-themes.tar.gz"
-	downloader "ubuntu-settings.tar.gz" "https://github.com/modded-ubuntu/modded-ubuntu/releases/download/config/ubuntu-settings.tar.gz"
-
-	echo -e "${R} [${W}-${R}]${C} Unpacking Files..\n"${W}
-	tar -xvzf fonts.tar.gz -C "/usr/local/share/fonts/"
-	tar -xvzf icons.tar.gz -C "/usr/share/icons/"
-	tar -xvzf wallpaper.tar.gz -C "/usr/share/backgrounds/xfce/"
-	tar -xvzf gtk-themes.tar.gz -C "/usr/share/themes/"
-	tar -xvzf ubuntu-settings.tar.gz -C "/home/$username/"	
-	rm -fr $temp_folder
-
-	echo -e "${R} [${W}-${R}]${C} Purging Unnecessary Files.."${W}
-	rem_theme
-	rem_icon
-
-	echo -e "${R} [${W}-${R}]${C} Rebuilding Font Cache..\n"${W}
-	fc-cache -fv
-
-	echo -e "${R} [${W}-${R}]${C} Upgrading the System..\n"${W}
-	apt update
-	yes | apt upgrade
-	apt clean
-	yes | apt autoremove
-
-}
-
-# ----------------------------
-
-check_root
+# Run
 package
-install_softwares
-config
-note
-
+distro
+sound
+permission
